5f1aa82f8fe3794f11e0418fb4d5fd4b8ca8c2bf
lucasbatt <lucas.battistella.00@gmail.com>
2019-04-07 20:00:18 -0300

    style.css tava errado

--- js/main.js
+++ js/main.js
@@ -5,16 +5,16 @@
     */
 
     /** Funcoes de efeito visual por mouseover mouseoff 
-     *  @param {div} x 
+     *  @param {Element} itemExib - incremento do slider 
      *  muda o estilo com ~inline css
      *  para mudar para outro estilo com css externo:
-     *  x.style = "estiloDesejado";
+     *  itemExib.style = "estiloDesejado";
      */
-    function changeTo(x) {
-        x.style.backgroundColor = "rgba(15, 119, 204, 0.548)";
+    function changeTo(itemExib) {
+        itemExib.style.backgroundColor = "rgba(15, 119, 204, 0.548)";
     }
-    function changeBack(x) {
-        x.style.backgroundColor = "rgba(255, 255, 255, 1)";
+    function changeBack(itemExib) {
+        itemExib.style.backgroundColor = "rgba(255, 255, 255, 1)";
     };
 
 
@@ -34,23 +34,35 @@
     /** Para garantir execuçao sequencial
      * todas as funcoes que operam sobre a pagina
      * sao filhas da que recebe os dados
-     * @param {objeto JSONP} data 
+     * @param {Object} resp - resposta do JSONP
+     * @param {Object} resp.data - contem 3 objetos:
+     * @param {Object} resp.data.widget - contem um {number} size 
+     * (provavelmente usado para definir quantas recomendacoes devem 
+     * ser exibidas)
+     * @param {Object} resp.data.reference - contem:
+     *                                              {string} businessID
+     *                                              {string} name
+     *                                              {string} imageName
+     *                                              {string} detailUrl
+     *                                              {string} price
+     *                                              {string} oldPrice
+     *                                              {string} product
      * (~ Linhas parecem iguais em secoes, 
      * talvez encapsular em uma funcao que seja chamada duas vezes ~)
      */
-var esperador = function(data) {
+var esperador = function(resp) {
 
     /*preenche referencia*/
-    var itemref = data.data.reference.item;
+    var itemref = resp.data.reference.item;
     var box = document.getElementById("reference");
     var templateitem = document.getElementById("templateitem").innerHTML;
     var html = Mustache.to_html(templateitem, itemref);
     box.innerHTML = html;
 
     /*preenche widget.size * recomendados*/
-    var itemrec = data.data.recommendation;
+    var itemrec = resp.data.recommendation;
     var holder = document.getElementById("recomendation");
-    for (i=0; i<data.data.widget.size; i++){
+    for (i=0; i<resp.data.widget.size; i++){
         var htmlcontent = Mustache.to_html(templateitem, itemrec[i]);
         holder.innerHTML =holder.innerHTML + htmlcontent;
     }
@@ -61,8 +73,6 @@ var esperador = function(data) {
     /** funcao que trata os botoes de navegacao
      *  @param {number} index
      * @param {number} increase
-     * @param {number} itemlen
-     * @param {number} telalen
      * Toda vez que clica um dos botoes
      * pega o tamanho do item
      * pega o tamanho do espaco dos recomendados que está sendo exibido
@@ -73,8 +83,8 @@ var esperador = function(data) {
     let liEls = document.querySelectorAll('ul li');
     let index = 0;
     window.show = function(increase) {
-        itemlen = document.getElementById("reference").clientWidth;
-        telalen = document.getElementById("recomendation").clientWidth;
+        var itemlen = document.getElementById("reference").clientWidth;
+        var telalen = document.getElementById("recomendation").clientWidth;
         if (increase > 0){
             index += Math.floor(telalen/itemlen);
         }
